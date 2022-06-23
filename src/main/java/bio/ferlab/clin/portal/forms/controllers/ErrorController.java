aadd5335ffd6096edad391d85f09285f564fd615
Yann MARCOU <creativeyann17@gmail.com>
2022-06-23 15:36:35 -0400

    feat: CLIN-971 - workflows + error handling

--- src/main/java/bio/ferlab/clin/portal/forms/controllers/ErrorController.java
+++ src/main/java/bio/ferlab/clin/portal/forms/controllers/ErrorController.java
@@ -2,6 +2,7 @@ package bio.ferlab.clin.portal.forms.controllers;
 
 import bio.ferlab.clin.portal.forms.exceptions.SecurityException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -9,6 +10,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 @Slf4j
 public class ErrorController {
+
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<String> handleException(Exception e) {
+    log.error("", e); // log everything
+    return new ResponseEntity<>("internal server error", HttpStatus.INTERNAL_SERVER_ERROR);
+  }
 
   @ExceptionHandler(SecurityException.class)
   public ResponseEntity<String> handleException(SecurityException e) {
--- src/main/java/bio/ferlab/clin/portal/forms/controllers/FormController.java
+++ src/main/java/bio/ferlab/clin/portal/forms/controllers/FormController.java
@@ -1,11 +1,9 @@
 package bio.ferlab.clin.portal.forms.controllers;
 
 import bio.ferlab.clin.portal.forms.clients.FhirClient;
-import bio.ferlab.clin.portal.forms.exceptions.SecurityException;
 import ca.uhn.fhir.rest.param.TokenParam;
 import org.hl7.fhir.r4.model.Patient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,7 +20,7 @@ public class FormController {
   public String config(@RequestParam(required = false) String lang, @PathVariable String formName) {
     final Locale locale = StringUtils.parseLocaleString(lang);
     Patient p = fhirClient.getClinClient().findPatientByMrn(new TokenParam(formName));
-    return p.getId();
+    return p.getIdBase();
   }
 
 }
